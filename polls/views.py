9279274e8f1de6d9755de810a82f37dcd73143f3
Soyon Lee <slee550@myseneca.ca>
2024-06-28 19:32:50 -0400

    Fixed expired question

--- polls/views.py
+++ polls/views.py
@@ -18,7 +18,7 @@ class IndexView(generic.ListView):
     # "latest_question_list"
     def get_queryset(self):
         """
-        Return the last five published questions (not including those set to be
+        Return the last 6 published questions (not including those set to be
         published in the future).
         """
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:6]
